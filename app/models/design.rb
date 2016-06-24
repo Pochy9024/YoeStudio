fd06a58cf46dac748a76d242f5fdf975eeb6b18f
Carlito <carlito@fec.uh.cu>
2016-06-24 09:47:35 -0400

    update all views in models (still missing update design/idex-view)

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,4 +1,4 @@
 class Design < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "450x450>", thumb: "100x100>", card: '1200x750>' }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, :styles => {:medium => '450x450!', :thumb => '100x100>', :card => '1200x750>' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
