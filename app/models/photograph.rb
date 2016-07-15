0fafa771f1b9e3baf6109aed50299b2a11ff4429
Jorge T <jotolo7@gmail.com>
2016-07-15 14:23:26 -0400

    changing quality for images

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -3,7 +3,13 @@ class Photograph < ActiveRecord::Base
   translates :technic, fallbacks_for_empty_translations: true
   accepts_nested_attributes_for :translations, allow_destroy: true
 
-  has_attached_file :image, styles: { medium: "450x450!", thumb: "100x100>", card: '1200x750!' }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "450x450!", thumb: "100x100>", card: '1200x750!' },
+                    :convert_options =>  {
+                        :medium => "-normalize -quality 60  -interlace Plane",
+                        :thumb => "-normalize -quality 50  -interlace Plane",
+                        :card => "-normalize -quality 60  -interlace Plane"
+                    },
+                    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   self.per_page = 6
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -3,6 +3,12 @@ class Slide < ActiveRecord::Base
   translates :title, :text, fallbacks_for_empty_translations: true
   accepts_nested_attributes_for :translations, allow_destroy: true
 
-  has_attached_file :slide, styles: { medium: "1920x700!", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :slide, styles: { medium: "1920x700!", thumb: "100x100>" },
+                    :convert_options =>  {
+                        :medium => "-normalize -quality 60  -interlace Plane",
+                        :thumb => "-normalize -quality 50  -interlace Plane",
+                        :card => "-normalize -quality 60  -interlace Plane"
+                    },
+                    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :slide, content_type: /\Aimage\/.*\Z/
 end
