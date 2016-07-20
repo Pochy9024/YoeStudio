42e0eaac35186521e3c7f173460153f0486a7c5b
Carlito <carlito@fec.uh.cu>
2016-07-20 11:35:28 -0400

    crop images

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -5,11 +5,11 @@ class Painting < ActiveRecord::Base
 
   validates_presence_of :position
 
-  has_attached_file :image, styles: { medium: "450x450!", thumb: "100x100>", card: '1200x750#' },
+  has_attached_file :image, styles: { medium: "450x450!", thumb: "100x100>", card: '1200x750^' },
                     :convert_options =>  {
                         :medium => "-normalize -quality 60  -interlace Plane",
                         :thumb => "-normalize -quality 50  -interlace Plane",
-                        :card => "-normalize -quality 60  -interlace Plane -gravity Center -crop 1200x750+0+0 "
+                        :card => "-normalize -quality 60  -interlace Plane -gravity Center -crop 1200x750+0+0 -resize 1200x750^"
                     },
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
